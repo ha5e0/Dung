e80468c5aeb9997cb789760f901bc479e5c06602
ha5e0 <sudoxxxx@gmail.com>
2015-04-24 10:43:26 +0200

    1

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraScript : MonoBehaviour {
+
+	public float LookSpeed = 5.0f;
+	public float ZoomZpeed = 5.0f;
+	public float ScrollSpeed = 1.0f;
+	public bool ScrollOnEdge = false;
+	public float ScrollEdge = 0.01f;
+	public float ScrollEdgeSpeed = 15.0f;
+
+	Vector2 lastMousePos;
+
+	// Use this for initialization
+	void Start () {
+		lastMousePos = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector2 mousePos = Input.mousePosition;
+		if ( Input.GetKey("mouse 1") )
+		{
+			transform.Translate(Vector3.right * Time.deltaTime * ScrollSpeed * (lastMousePos.x-mousePos.x), Space.Self);
+			Vector3 fwd = transform.forward;
+			fwd.y = 0;
+			fwd.Normalize();
+			transform.Translate(fwd * Time.deltaTime * ScrollSpeed * (lastMousePos.y-mousePos.y), Space.World);
+			
+		}
+		if ( Input.GetKey("mouse 2") )
+		{
+			transform.Rotate(Vector3.down * Time.deltaTime * LookSpeed * (lastMousePos.x-mousePos.x), Space.World);
+			transform.Rotate(Vector3.right * Time.deltaTime * LookSpeed * (lastMousePos.y-mousePos.y), Space.Self);
+		}
+		lastMousePos = mousePos;
+
+		transform.Translate (Vector3.forward * ZoomZpeed * Input.GetAxis ("Mouse ScrollWheel"), Space.Self);
+
+		if (ScrollOnEdge) {
+			if (Input.GetKey ("d") || Input.mousePosition.x >= Screen.width * (1 - ScrollEdge)) {
+				transform.Translate (Vector3.right * Time.deltaTime * ScrollEdgeSpeed, Space.Self);
+			} else if (Input.GetKey ("a") || Input.mousePosition.x <= Screen.width * ScrollEdge) {
+				transform.Translate (Vector3.right * Time.deltaTime * -ScrollEdgeSpeed, Space.Self);
+			}
+		
+			if (Input.GetKey ("w") || Input.mousePosition.y >= Screen.height * (1 - ScrollEdge)) {
+				Vector3 fwd = transform.forward;
+				fwd.y = 0;
+				fwd.Normalize ();
+				transform.Translate (fwd * Time.deltaTime * ScrollEdgeSpeed, Space.World);
+			} else if (Input.GetKey ("s") || Input.mousePosition.y <= Screen.height * ScrollEdge) {
+				Vector3 fwd = transform.forward;
+				fwd.y = 0;
+				fwd.Normalize ();
+				transform.Translate (fwd * Time.deltaTime * -ScrollEdgeSpeed, Space.World);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	
+	public float speed = 1.0f;
+	
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float hor = Input.GetAxis ("Horizontal");
+		float ver = Input.GetAxis ("Vertical");
+		
+		Vector3 forwardDirection = Camera.main.transform.TransformDirection (Vector3.forward);
+		forwardDirection.y = 0;
+		forwardDirection.Normalize ();
+		forwardDirection *= Input.GetAxis ("Vertical");
+		
+		Vector3 rightDirection = Camera.main.transform.TransformDirection (Vector3.right);
+		rightDirection.y = 0;
+		rightDirection.Normalize ();
+		rightDirection *=Input.GetAxis ("Horizontal");
+
+		GetComponent<Rigidbody>().MovePosition(transform.position + (forwardDirection + rightDirection) * speed * Time.deltaTime);
+
+		
+		if (Input.GetAxis("Horizontal")!=0 || Input.GetAxis("Vertical")!=0) transform.forward = Vector3.Normalize(forwardDirection + rightDirection);
+		
+		
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GetComponent<Rigidbody>().AddForce(new Vector3(0,300.0f,0));
+		}
+
+		
+		//Vector3 moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+		//GetComponent<Rigidbody>().MovePosition(transform.position + moveDirection * speed * Time.deltaTime);
+	}
+	
+	void FixedUpdate() {
+	}
+}
